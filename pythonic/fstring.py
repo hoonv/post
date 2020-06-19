e26b016ddc9b6069a4a17bd7749d3132a9eddfdd
chaechef <chaeefe@gmail.com>
2020-06-19 13:15:23 +0900

    fstring

--- pythonic/fstring.py
+++ pythonic/fstring.py
@@ -3,9 +3,14 @@
 #     print(f"i'm {data[0]} and {data[1]} years old")
 #     # i'm john and 12 years old
 
-if __name__ == "__main__":
-    a = 1000
-    b = 10
+# if __name__ == "__main__":
+#     a = 1000
+#     b = 10
+#
+#     a, b = b, a
+#     print(a,b)
 
-    a, b = b, a
-    print(a,b)
+if __name__ == "__main__":
+    arr = [2, 1, 4, 3, 5]
+    arr = [[1,2], [4,3,3]]
+    print(list(map(len, arr)))
